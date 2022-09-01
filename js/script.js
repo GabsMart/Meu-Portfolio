327a079551bf0c4ee563ee3c30d5dba23d1a5452
GabsMart <GabrielMartins014@hotmail.com>
2022-09-01 10:22:26 -0300

    - Adicionado aba de habilidades e formação
    - Adicionado aba com informação sobre habilidades e formações acadêmicas;
    - Removido imagem do banner principal;
    - Adicionado efeito de Hover na navegação;
    - Navegação agora leva o usuário até a aba clicada;
    - Alterado espaçamento na navegação;
    - Adicionado titulo ao Website.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ botãoPulseira.addEventListener("click", function () {
 fecharVideo.addEventListener("click", function () {
     videoYT.style.display = 'none';
 })
+
+// Fazer Habilidades ou Formação Acadêmica aparecer na aba #Habilidades
+let aparecerHabilidades = document.getElementById('aparecer-Habilidades');
+let aparecerFormação = document.getElementById('aparecer-Formação')
+
+let conteudoHabilidades = document.getElementById('Habilidades');
+let conteudoFormação = document.getElementById('Formação');
+
+aparecerHabilidades.addEventListener("click", function () {
+    conteudoHabilidades.style.display = 'grid';
+    conteudoFormação.style.display = 'none';
+})
+
+aparecerFormação.addEventListener("click", function () {
+    conteudoFormação.style.display = 'grid';
+    conteudoHabilidades.style.display = 'none';
+})
